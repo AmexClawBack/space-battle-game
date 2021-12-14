c1dd4e800351d97c907a2ec3c8304898d756d729
Dillon King <Dillonk2012@gmail.com>
2021-12-13 18:55:05 -0600

    Completed functionality of game

--- js/script.js
+++ js/script.js
@@ -1,69 +1,50 @@
 class SpaceShip {
-    constructor (name, hull, firepower, accuracy) {
-        this.name = name;
+    constructor (hull, firepower, accuracy) {
         this.hull = hull;
         this.firepower = firepower;
         this.accuracy = accuracy;
     }
-    // Methods
-
-    // Attack
-    //my attack power minus enemies hull
-    // use math.random to measure against attack power && accuracy.
-    // if accuracy > the random number THEN 
-    attackEnemy() {
-        
-        console.log()
-    }
-    // Dogfight
-    attackMe() {
-        
-    }
-    // Im hit
-    // After hit, subtract enemies firepower from current hull and return myShips hull && enemyShips hull. we want to display both hp's after each attack.
-    imHit() {
-        // if (Math.random() < enemyOne.accuracy) {
-        //     console.log('You have been hit!');
-        //     return myShip.hull = myShip.hull - enemyOne.firepower;
-        //     console.log( this.myShip.hull);
-        // }
-    }
-    // Retreat
-    retreat() {
-        
+    attack(target) {
+        target.hull = target.hull - (this.firepower * this.accuracy);
     }
     
 }
-class enemyShips extends SpaceShip {
-        constructor(name, hull, firepower, accuracy) {
-                this.name = name,
-                this.hull = hull,
-                this.firepower = firepower,
-                this.accuracy = accuracy
-        
-                super(name, hull, firepower, accuracy)
-            }
-        };
-        
-        // for (let i = 0; i < enemyShips.length; i++) {
-            //     console.log(i)
-            // } // figure out how to run a for loop to generate enemy ships into array without hardcoding it
+
+class Usscp extends SpaceShip {
+    constructor(hull, firepower, accuracy) {
+        super(hull, firepower, accuracy)
+    }
+}
+class Alien extends SpaceShip {
+    constructor(hull, firepower, accuracy) {
+        super(hull, firepower, accuracy)
+    }
+}
+
+const alien = [];
+
+for(let i = 0; i < 6; i++) {
+    alien[i] = new Alien(4, 4, 0.7)
+}
+
+const captain = new Usscp(20, 5, 0.7)
+
+function startGame (arr, obj) {
+    let i = 0;
+    while (i < arr.length && arr[i].hull > 0) {
+        // You attack the first alien ship
+        obj.attack(arr[i])
+        if(arr[i].hull > 0) { //alien survives, attacks player
+            arr[i].attack(obj);
             
-            const myShip = new SpaceShip('USS Hello World', 20, 5, .7)
-            console.log(myShip.name);
-            console.log(myShip.accuracy)
-            
-            const enemyOne = new SpaceShip('text1', 6, 4, .8);
-            const enemyTwo = new SpaceShip('text2', 6, 4, .8);
-            const enemyThree = new SpaceShip('text3', 6, 4, .8);
-            const enemyFour = new SpaceShip('text4', 6, 4, .8);
-            const enemyFive = new SpaceShip('text5', 6, 4, .8);
-            const enemySix = new SpaceShip('text6', 6, 4, .8);
+        if(obj.hull < 0) {
+            return "You Lost!"
+        } i += 1;
+       return "You Won!"
+        }
+    }
+}
+
+console.log(startGame(alien, captain));
 
 
-            // Enemy ships
-            const enemies = [enemyOne, enemyTwo, enemyThree, enemyFour, enemyFive, enemySix];
-
-            console.log(enemies);
-
-            myShip.imHit()
