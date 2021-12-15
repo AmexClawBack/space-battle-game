fc2cfc78feae3b0d7b4b1436f0940cba074425a5
Dillon King <Dillonk2012@gmail.com>
2021-12-14 20:23:56 -0600

    logic for attack sequence added for myship and alienship

--- newscript.js
+++ newscript.js
@@ -4,8 +4,10 @@ const myShip = {
     firepower: 5,
     accuracy: .7,
     attack(target) {
-        target.hull = target.hull - this.firepower;
+        if (Math.random() < this.accuracy) {
+        target.hull = (target.hull - this.firepower)
         console.log(`You have hit the enemy ship for ${this.firepower} points, leaving them with ${target.hull} health points.`)
+        }else console.log("You have missed your target")
     }
 }
 
@@ -14,18 +16,22 @@ const alienShip = {
     firepower: 5,
     accuracy: .7,
     attack(target) {
-        target.hull = target.hull - this.firepower;
-        console.log(`You have been hit the by enemy ship for ${this.firepower} points, leaving you with ${target.hull} health points.`)
+        if (Math.random() < this.accuracy) {
+        target.hull = (target.hull - this.firepower)
+        console.log(`You have been hit by the enemy ship for ${this.firepower} points, leaving you with ${target.hull} health points.`)
+        }else console.log("Your enemy missed")
     }
 
-
 }
-myShip.attack(alienShip)
 
-const game = {
-    function(target) {
+
+const game = (target) => {
         if (target.hull <= 0) {
             alert("That ship exploded")
-        }
+            console.log('Target ship is destroyed')
+        }else console.log('The fight continues')
     }
-}
+
+    myShip.attack(alienShip)
+    game(alienShip)
+    alienShip.attack(myShip)
