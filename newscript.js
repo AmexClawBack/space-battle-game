4f2ebbd19a745911a3d7463658fdbcc257e3d572
Dillon King <Dillonk2012@gmail.com>
2021-12-14 20:34:10 -0600

    Working commit before turning objects into classes

--- newscript.js
+++ newscript.js
@@ -8,13 +8,16 @@ const myShip = {
         target.hull = (target.hull - this.firepower)
         console.log(`You have hit the enemy ship for ${this.firepower} points, leaving them with ${target.hull} health points.`)
         }else console.log("You have missed your target")
+        if (this.hull <= 0) {
+            alert("You Lose!")
+        }
     }
 }
 
 const alienShip = {
-    hull: 20,
-    firepower: 5,
-    accuracy: .7,
+    hull: Math.floor(Math.random() * 4) + 3,
+    firepower: Math.floor(Math.random() * 3) + 2,
+    accuracy: (Math.floor(Math.random() * 3) + 6) / 10,
     attack(target) {
         if (Math.random() < this.accuracy) {
         target.hull = (target.hull - this.firepower)
@@ -28,10 +31,10 @@ const alienShip = {
 const game = (target) => {
         if (target.hull <= 0) {
             alert("That ship exploded")
-            console.log('Target ship is destroyed')
         }else console.log('The fight continues')
     }
 
-    myShip.attack(alienShip)
-    game(alienShip)
-    alienShip.attack(myShip)
+    myShip.attack(alienShip);
+    game(alienShip);
+    alienShip.attack(myShip);
+    game(myShip);
